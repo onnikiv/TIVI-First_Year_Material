afc620cf271dd70c3d8b07460ef19b02771a0762
onnik <onni.kivinen@gmail.com>
2024-01-16 19:37:31 +0200

    commit // added tehtävä3.py

---
+++ python/moduuli2/tehtävä3.py
@@ -0,0 +1,6 @@
+kanta = input("Anna suorakulmion kanta: \n")
+korkeus = input("Anna suorakulmion korkeus: \n")
+pintaA = int(kanta) * int(korkeus)
+piiri = int(kanta) * 2 + int(korkeus) * 2
+print("Suorakulmion pinta-ala on: " + str(pintaA) + " ja piiri on: " + str(piiri) + ".")
+
