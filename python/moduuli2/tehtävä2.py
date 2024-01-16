5e63b2319fd7da13a7256d79757d1e3fbf856c1e
onnik <onni.kivinen@gmail.com>
2024-01-16 21:17:12 +0200

    commit

--- python/moduuli2/tehtävä2.py
+++ python/moduuli2/tehtävä2.py
@@ -1,5 +1,5 @@
 import math
 
-säde = input("Mikä on ympyrän säteen pituus?: \n")
-ala = str(math.pi * pow(float(säde),2))
+rad = input("Mikä on ympyrän säteen pituus?: \n")
+ala = str(math.pi * pow(float(rad), 2))
 print("Ympyrän pinta-ala on tällöin " + ala)
