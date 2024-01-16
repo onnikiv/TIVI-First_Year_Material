c4fe12e904db092a532d58bfc88a395b8c0034a7
onnik <onni.kivinen@gmail.com>
2024-01-16 20:08:05 +0200

    commit

---
+++ python/moduuli2/tehtävä 4.py
@@ -0,0 +1,10 @@
+luku1 = input("Anna luku\n")
+luku2 = input("Anna toinen luku\n")
+luku3 = input("Anna vielä kolmas luku\n")
+
+summa = int(luku1) + int(luku2) + int(luku3)
+print("Summa on: " +str(summa))
+tulo = int(luku1) * int(luku2) * int(luku3)
+print("Tulo on: " +str(tulo ))
+keskiarvo = int(summa) / 3
+print("Keskiarvo on: " +str(keskiarvo))
