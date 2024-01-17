ad8664d165cfdd584f4bcb8bdceb9efc1593fcd3
onnik <onni.kivinen@gmail.com>
2024-01-17 20:23:00 +0200

    commit

--- python/moduuli2/tehtävä2.py
+++ python/moduuli2/tehtävä2.py
@@ -1,5 +1,4 @@
 import math
-
 rad = input("Mikä on ympyrän säteen pituus?: \n")
 ala = str(math.pi * pow(float(rad), 2))
 print("Ympyrän pinta-ala on tällöin " + ala)
---
+++ python/moduuli2/tehtävä5.py
@@ -0,0 +1,7 @@
+print("Anna paino")
+
+leivis = float(input("Leiviskät: "))
+naula = float(input("Naulat: "))
+luoti = float(input("Luodit: "))
+paino = 13.3 * ((leivis * 20 + naula) * 32 + luoti)
+print("Massa nykymittojen mukaan: " + str(int(paino / 1000)) + " kiloa " + str(int(paino % 1000))+ " grammaa.")
