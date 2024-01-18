a3de423efa6d8cefcc73021e8b3ec623b28922e7
onnik <onni.kivinen@gmail.com>
2024-01-18 15:14:40 +0200

    commit

---
+++ python/kokeilut/katkoviivatesti.py
@@ -0,0 +1,12 @@
+print("-" * 80 + "\n" + "Minun nimeni on Onni \n" + "-" * 80)
+
+print(10 / 3)
+print(10// 3)
+print(10 % 3)
+
+luku = 3
+# merkkijono = mä asun osoitteessa kuninkaan katu 3
+print("mä asun kuninkaan katu " + str(luku))
+
+print(2, "hello", True, [1,3,8])
+
---
+++ python/kokeilut/tuuma - cm.py
@@ -0,0 +1,11 @@
+tuuma_str = float(input("Anna mitta tuumana \n"))
+cm = 2.54 * tuuma_str
+print(f"senttimetreinä: {cm}")
+
+# desimaaleja kaksi piissä
+import math
+munpi = math.pi
+print(f"{munpi:.2f}") # .2f kertoo kuinka monta desimaalia printataan
+print(f"{munpi:10.5f}") # 10 kertoo kuinka suureen kenttään se printtaa komennon
+print(f"{munpi:20.5f}")
+print(f"{munpi:30.2f}")
