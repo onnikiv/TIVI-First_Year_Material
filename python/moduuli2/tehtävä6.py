412ff69a0d96570536119da5591c0c2a9f90d8bc
onnik <onni.kivinen@gmail.com>
2024-01-18 09:43:26 +0200

    commit // tehtävä6.py

---
+++ python/moduuli2/tehtävä6.py
@@ -0,0 +1,5 @@
+import random
+Random_kolmiluku = random.randint(0, 999)
+print("Kolmiluku: " + str(Random_kolmiluku) + ".")
+Random_neliluku = random.randint(0, 9999)
+print("Neliluku: " + str(Random_neliluku) + ".")
