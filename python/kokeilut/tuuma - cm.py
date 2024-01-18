a7304595d2c1e489ace6960b850f0d5120d737bb
onnik <onni.kivinen@gmail.com>
2024-01-18 15:19:55 +0200

    commit

--- python/kokeilut/tuuma - cm.py
+++ python/kokeilut/tuuma - cm.py
@@ -9,3 +9,5 @@ print(f"{munpi:.2f}") # .2f kertoo kuinka monta desimaalia printataan
 print(f"{munpi:10.5f}") # 10 kertoo kuinka suureen kenttään se printtaa komennon
 print(f"{munpi:20.5f}")
 print(f"{munpi:30.2f}")
+print(f"{munpi:>50.2f}")
+print(f"{munpi:<50.2f}")
