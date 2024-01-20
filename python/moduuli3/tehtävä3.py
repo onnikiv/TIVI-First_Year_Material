b95c1407af1d16bacf23816d07f42dfb2cbb5095
onnik <onni.kivinen@gmail.com>
2024-01-20 17:57:37 +0200

    commit // tehtävä2.py

---
+++ python/moduuli3/tehtävä3.py
@@ -0,0 +1,5 @@
+harvo = float(input("syötä hemogloniiniarvo: \n"))
+if 117 <= harvo <= 175:
+    print("Naiselle hemogloniiniarvot ovat normaalit.")
+else 117 < harvo :
+    print("Naiselle hemogloniiniarvot ovat alhaiset")
