dd69b2af84b40f95ed3299df7f5e86cdc5e801ac
onnik <onni.kivinen@gmail.com>
2024-01-28 22:47:18 +0200

    tehtävä4.py

--- python/moduuli4/tehtävä4.py
+++ python/moduuli4/tehtävä4.py
@@ -1,8 +1,10 @@
 import random
-
-randomi_luku = random.randint(1, 10)
-luku_input = int(input(f"Anna luku väliltä 1-10 \n"))
-while randomi_luku == luku_input:
-    print("Oikea vastaus:")
-    if luku_input <= randomi_luku:
-        print("Liian korkea")
+numero = random.randint(1,10)
+arvaus = int(input('Arvaa numero: '))
+while arvaus != numero:
+    if arvaus < numero:
+        print('Liian pieni arvaus.')
+    elif arvaus > numero:
+        print('Liian suuri arvaus.')
+    arvaus = int(input('Arvaa numero: '))
+print('Oikea arvaus')
