e4dd96f6bae6961a822e0703f54ac9b52817eaf5
onnik <onni.kivinen@gmail.com>
2024-01-30 19:54:59 +0200

    added tehtävä1.py & tehtävä2.py

---
+++ python/moduuli5/tehtävä1.py
@@ -0,0 +1,15 @@
+import random
+
+arpakuutioiden_lukum = int(input("Anna arpakuutioiden lukumäärä: \n"))
+
+if arpakuutioiden_lukum <= 0:
+    print("Arpakuutioiden lukumäärän pitää olla positiivinen.")
+else:
+    tuloksen_summa = 0
+    for i in range(arpakuutioiden_lukum):
+        silmaluku = random.randint(1,6)
+        print(f"Heitto: {silmaluku}")
+        tuloksen_summa += silmaluku
+
+print(f"Heitettyjen arpakuutioiden summa: {tuloksen_summa}")
+
