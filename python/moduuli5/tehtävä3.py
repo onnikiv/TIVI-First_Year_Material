b225883f2049ee7fc440d44f67f5fe073815a086
onnik <onni.kivinen@gmail.com>
2024-02-01 19:08:01 +0200

    added tehtävä3.py and tehtävä4.py

--- python/moduuli5/tehtävä3.py
+++ python/moduuli5/tehtävä3.py
@@ -1,4 +1,16 @@
-luku_syote = input("Luku syote: ")
+luku_string = input("Anna luku: \n")
+alkuluku = 0
+if luku_string.isdigit():   #Tarkistetaan syöttö, jos luku niin tehdään merkkijonosta(str) luku(int).
+    luku = int(luku_string)
 
-if luku_syote.isdigit():
-    luku = int(luku_syote)
+    if luku < 2:    #Jos alle 2 ei voi olla alkuluku
+        print(f"{luku} ei ole alkuluku.")
+    else:
+        alkuluku = True  # Aluksi alkuluku on totta.
+        for i in range(2, int(luku ** 0.5) + 1):
+            if luku % i == 0:
+                alkuluku = False
+                break
+
+    if alkuluku: # Jos alkuluku, niin printataan.
+        print(f"{luku} on alkuluku.")
