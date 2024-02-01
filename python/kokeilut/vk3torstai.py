afe152adc8507b400190c5933f9d23fe4eead161
onnik <onni.kivinen@gmail.com>
2024-02-01 14:02:51 +0200

    add

---
+++ python/kokeilut/vk3torstai.py
@@ -0,0 +1,13 @@
+# # MONIKKO, JOUKKO JA SANAKIRJA
+#
+# munmonikko = (1,3,5,9,12)
+# print(munmonikko[2:])
+#
+# munmonikko = (1, 3, 5)
+# eka, toka, kolmas = munmonikko
+# print(toka)
+
+ mun1 = (1,2,3)
+ mun1.append(5)
+ print(mun1)
+ # EI TOIMI KOSKA TUPLE, EI voi lisätä tupleen
--- python/moduuli5/tehtävä3.py
+++ python/moduuli5/tehtävä3.py
@@ -1,14 +1,4 @@
-alkuluvut = []
-luku = 0
-luku_syote = int(input("Anna luku: \n"))
-if (luku_syote / luku_syote) == 0:
-    if luku_syote % 1 == 0:
+luku_syote = input("Luku syote: ")
 
-    alkuluvut.append(luku)
-    if luku_syote == "":
-        print("Virheellinen syöte.")
-    else:
-        print("Tämä ei ole alkuluku")
-
-
-print(f"{luku} on alkuluku")
+if luku_syote.isdigit():
+    luku = int(luku_syote)
