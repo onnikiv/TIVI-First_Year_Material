01dba22b40d6c8327b611c77d5f9381b35de0472
onnik <onni.kivinen@gmail.com>
2024-02-02 18:22:18 +0200

    tweak

--- python/moduuli6/tehtävä1.py
+++ python/moduuli6/tehtävä1.py
@@ -6,9 +6,11 @@ def nopanheitto(): #Funktio yksittäiselle nopanheitolle
 
 
 def haluttu_kutonen(): #Pyöritetään siihen asti kun tulee kuusi.
+    heiton_numero = 1 #Alustetaan heitot numeroidusti
     while True:
         heitto = nopanheitto()
-        print(f"Nopan silmäluku: {heitto}")
+        print(f"Heitto {heiton_numero}, Nopan silmäluku: {heitto}")
+        heiton_numero += 1
         if heitto == 6: #Kun tulee luvuksi kuusi -> print ja break
             print(f"Katohan tulihan se kutonen!")
             break
