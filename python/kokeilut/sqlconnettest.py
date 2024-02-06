472a977eafd79a930dd376e3aab13360512d5871
onnik <onni.kivinen@gmail.com>
2024-02-06 11:28:14 +0200

    added sqlconnettest.py

--- python/kokeilut/sqlconnettest.py
+++ python/kokeilut/sqlconnettest.py
@@ -23,3 +23,42 @@ if cursor.rowcount > 0:
 
 else:
     print("Ei tuloksia")
+
+#Parempi tapa: kääritään yksittäiset toimimallisuudet uudelleenkäytettäviin funktioihin
+
+
+def find_country_by_code(iso_code):
+    sql = f"SELECT name, continent FROM country WHERE iso_country='{iso_code}'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    if cursor.rowcount == 1:
+        return result
+    return
+
+
+def update_country_name_by_code(iso_code, name):
+    sql = f"UPDATE country SET name='{name}' WHERE iso_country='{iso_code}'"
+    #tarkista, että sql on oikein muodostettu
+    #print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    if cursor.rowcount == 1:
+        print(f"Tietue päivitetty ({iso_code}:{name}).")
+        return True
+    print("Pieleen meni.")
+
+
+code_input = input("Anna Maakoodi: ")
+country_input = input("Uusi nimi: ")
+update_country_name_by_code(code_input, country_input)
+
+
+user_input = input("Anna Maakoodi: ")
+country = find_country_by_code(user_input)
+# Jos country != None
+if country:
+    print(f"Löydetty maa: {country[0]}, {country[1]}")
+else:
+    print("Ei tuloksia.")
+
