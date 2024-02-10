e54a8451929fdfbe9de417e5b18f7367e542de47
onnik <onni.kivinen@gmail.com>
2024-02-10 16:56:46 +0200

    tweak

---
+++ python/moduuli8/tehtävä1.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+    host="localhost",
+    port="3306",
+    database="flight_game",
+    user="root",
+    password="root",
+    autocommit=True
+    )
+    
