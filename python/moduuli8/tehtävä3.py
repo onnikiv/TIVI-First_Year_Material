01662417644d72813b885def707eee375a790832
onnik <onni.kivinen@gmail.com>
2024-02-11 18:00:18 +0200

    finished tehtävä3.py

--- python/moduuli8/tehtävä3.py
+++ python/moduuli8/tehtävä3.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from geopy.distance import geodesic
 
 connection = mysql.connector.connect( #Yhteys tietokantaan.
     host="localhost",
@@ -8,3 +9,17 @@ connection = mysql.connector.connect( #Yhteys tietokantaan.
     password="root",
     autocommit=True
 )
+
+
+def coordinate_search(ICAO):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident='{ICAO}'")
+    result = cursor.fetchone()
+    return result
+
+Icao_1_coordinate = coordinate_search(input("Anna ensimmäinen ICAO-koodi:\n "))
+Icao_2_coordinate = coordinate_search(input("Anna toinen ICAO-koodi:\n "))
+
+distance = geodesic(Icao_1_coordinate, Icao_2_coordinate).kilometers
+
+print(f"Etäisyys kahden lentokentän välillä on {distance:.2f} km.")
