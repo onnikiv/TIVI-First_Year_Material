9b1d1d2c90acaa90bd4e2baf20594f06abcb7616
onnik <onni.kivinen@gmail.com>
2024-02-25 17:09:38 +0200

    reformatointi

--- python/kokeilut/randomshiz.py
+++
@@ -1,7 +0,0 @@
-import random
-
-sukupuoli = random.randint(0, 1)
-if sukupuoli == 0:
-    print("Onneksi olkoon se on poika.")
-else:
-    print("Onneksi olkoon se on tyttö")
--- python/kokeilut/vk2 tuntishiz.py
+++ python/kokeilut/vk2 tuntishiz.py
@@ -10,3 +10,11 @@ if (ikä >= 18 or (ikä >= 15 and paino >= 55)):
     print("Lääkkeen käyttö on sallittua.")
 else:
     print("Lääkkeen käyttö on kielletty")
+
+import random
+
+sukupuoli = random.randint(0, 1)
+if sukupuoli == 0:
+    print("Onneksi olkoon se on poika.")
+else:
+    print("Onneksi olkoon se on tyttö")
