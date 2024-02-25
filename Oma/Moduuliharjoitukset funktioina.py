f843517c9ec91d2f4f09dee9fb682e0dea1110aa
onnik <onni.kivinen@gmail.com>
2024-02-25 20:38:25 +0200

    added moduuli 3, tehtävä 2

--- Oma/Moduuliharjoitukset funktioina.py
+++ Oma/Moduuliharjoitukset funktioina.py
@@ -1,4 +1,5 @@
 # ---------------------------------------------------------------------------------------------------------
+
 # MODUULI 2, TEHTÄVÄ 1 
 def tervehdys():
     nimi = input("Anna nimesi: \n")
@@ -48,7 +49,6 @@ massat_kilogrammoiksi()
 
 
 # MODUULI 2, TEHTÄVÄ 6 
-
 import random
 
 def random_kolmiluku():
@@ -68,10 +68,26 @@ random_neliluku()
 
 # ---------------------------------------------------------------------------------------------------------
 # MODUULI 3, TEHTÄVÄ 1 
-
 def kuha_tehtava():
     kuhan_pituus = float(input("Anna kuhan pituus senttimetreinä: \n"))
     if kuhan_pituus < 37:
         print(f"Kuha on alimittainen. Laske kuha takaisin järveen. Kuha on {37-kuhan_pituus} liian lyhyt. ")
 
 kuha_tehtava()
+
+# MODUULI 3, TEHTÄVÄ 2
+
+def hyttiluokat():
+    syotto = input("Anna hyttiluokka: \n").upper()
+    if syotto == "LUX":
+        print("LUX on parvekkeellinen hytti yläkannella..")
+    elif syotto == "A":
+        print("A on ikkunallinen hytti autokannen yläpuolella.")
+    elif syotto == "B":
+        print("B on ikkunaton hytti autokannen yläpuolella.")
+    elif syotto == "C":
+        print("C on ikkunaton hytti autokannen alapuolella.")
+    else:
+        print("Virheellinen syöttö.")
+
+hyttiluokat()
