ae8d3a7b754e58c732608b09479c880642107cb4
onnik <onni.kivinen@gmail.com>
2024-02-26 17:52:13 +0200

    Add functions for tasks 3 and 4

--- Oma/Moduuli 3 harjoitukset funktioina.py
+++ Oma/Moduuli 3 harjoitukset funktioina.py
@@ -1,5 +1,6 @@
 # ---------------------------------------------------------------------------------------------------------
 # MODUULI 3, TEHTÄVÄ 1 
+"""
 def kuha_tehtava():
     kuhan_pituus = float(input("Anna kuhan pituus senttimetreinä: \n"))
     if kuhan_pituus < 37:
@@ -23,4 +24,31 @@ def hyttiluokat():
         print("Virheellinen hyttiluokka.")
 
 hyttiluokat()
+"""
 
+# MODUULI 3, TEHTÄVÄ 3
+
+def hemoglobiini_arvo():
+    sukupuoli = input("Anna sukupuoli: \n").lower().capitalize()
+    hemoglobiini_input = float(input("Anna hemoglobiini: \n"))
+    if sukupuoli is not str and hemoglobiini_input is not float:
+        print("Virheellinen syöte.")
+    if sukupuoli == "Mies":
+        if hemoglobiini_input < 134:
+            print("Hemoglobiiniarvo miehelle on alhainen.")
+        elif hemoglobiini_input > 170:
+            print("Hemoglobiiniarvo miehelle on korkea.")
+        else:
+            print("Hemoglobiiniarvo miehelle on normaali.")
+    elif sukupuoli == "Nainen":
+        if hemoglobiini_input < 117:
+            print("Hemoglobiiniarvo naiselle on alhainen.")
+        elif hemoglobiini_input > 157:
+            print("Hemoglobiiniarvo naiselle on korkea.")
+        else:
+            print("Hemoglobiiniarvo naiselle on normaali.")
+
+hemoglobiini_arvo()
+
+
+# MODUULI 3, TEHTÄVÄ 4
