226f575509ebb24add269ce9fb2834789b5c429e
onnik <onni.kivinen@gmail.com>
2024-02-26 13:46:22 +0200

    added koe.py

---
+++ Oma/koe.py
@@ -0,0 +1,42 @@
+"""
+n = 13
+if n>=10:
+    print("A")
+    if n>=15:
+        print("B")
+    else:
+        print("C")
+else:
+    print("D")
+print("E")
+
+
+i = 1 # 1, 
+while i%5 > 0:
+    print(i)
+    i = 1+ 3
+print(i)
+
+for a in range(5,1-1):
+    print(f"{a} print")
+
+people = ("Mary", "Ahmed", "Pekka", "Olga")
+olga, pekka, mary = people
+
+
+
+
+
+def bytes_to_mega_bytes(bytes):
+    return bytes / (1024 * 1024)
+
+def double(numbers):
+    doubled = []
+    for a in numbers:
+        doubled.append(2 * a)
+    return doubled
+
+#ARVOSANAT pisteillä
+# 9 = 1, 12 = 2, 15 = 3 18 = 4, 21 = 5
+
+"""
