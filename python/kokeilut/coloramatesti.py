693ec149e1f2ff76fef2619b57010d04b0fb0fca
onnik <onni.kivinen@gmail.com>
2024-02-26 13:58:30 +0200

    merge & added coloramatesti.py

---
+++ python/kokeilut/coloramatesti.py
@@ -0,0 +1,6 @@
+import colorama
+from colorama import Fore, Style
+
+print(Fore.RED + "Tämä teksti on punaista")
+print(Fore.GREEN + "Tämä teksti on vihreää")
+print(Style.RESET_ALL)  # Palauta teksti alkuperäiseen väriin
