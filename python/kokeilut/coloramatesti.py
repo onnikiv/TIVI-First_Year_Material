24117c06d83927dbba80af2ed947d4b7c139717f
onnik <onni.kivinen@gmail.com>
2024-02-26 13:59:16 +0200

    tweak

--- python/kokeilut/coloramatesti.py
+++ python/kokeilut/coloramatesti.py
@@ -1,6 +1,5 @@
-import colorama
-from colorama import Fore, Style
-
-print(Fore.RED + "Tämä teksti on punaista")
-print(Fore.GREEN + "Tämä teksti on vihreää")
-print(Style.RESET_ALL)  # Palauta teksti alkuperäiseen väriin
+""""
+from colorama import Fore, Style, init 
+init(convert=True)
+print(Fore.RED + "Hello World")
+"""
