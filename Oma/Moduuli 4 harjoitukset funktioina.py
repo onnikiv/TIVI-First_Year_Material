d594c362327873c65a3fb1a2dcc532667b93ac50
onnik <onni.kivinen@gmail.com>
2024-02-28 18:58:48 +0200

    Add functions for exercises 1, 2, and 3

--- Oma/Moduuli 4 harjoitukset funktioina.py
+++ Oma/Moduuli 4 harjoitukset funktioina.py
@@ -1,11 +1,14 @@
+# MODUULI 4, TEHTÄVÄ 1
+
 def kolmella_jaolliset_alle_tuhat():
     i = 0
     while i < 1000:
         print(f"{i}")
         i = i + 3
 
-#kolmella_jaolliset_alle_tuhat()
+# kolmella_jaolliset_alle_tuhat()
 
+# MODUULI 4, TEHTÄVÄ 2
 def tuuma_cm():
     tuuma = 2.54
     tuuma_input = 0
@@ -15,5 +18,15 @@ def tuuma_cm():
             break
         print(f"Senttimetreinä: {tuuma_input * tuuma}.")
     
-tuuma_cm()
+# tuuma_cm()
 
+# MODUULI 4, TEHTÄVÄ 3
+
+def kysytaan_lukuja():
+    while True:
+        luku_input = int(input("Anna luku: \n"))
+        if luku_input == str:
+            break
+        print(f"Luku: {luku_input}.")
+
+kysytaan_lukuja()
