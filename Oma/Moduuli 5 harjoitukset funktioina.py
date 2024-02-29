b88971052e5493c4404bbc9a654661c397b06f46
onnik <onni.kivinen@gmail.com>
2024-02-29 16:58:23 +0200

    Refactor multiple dice roll function in Moduuli 5 harjoitukset funktioina.py

--- Oma/Moduuli 5 harjoitukset funktioina.py
+++ Oma/Moduuli 5 harjoitukset funktioina.py
@@ -1,11 +1,27 @@
+# ---------------------------------------------------------------------------------------------------------
+
+# MODUULI 5, TEHTÄVÄ 1 
+
 def useampi_arpakuutio():
     
     import random
-    noppien_summa = []
+    
+    tuloksen_summa = 0      # alustettu nollaksi
+    arpakuutioiden_lukum = int(input("Anna arpakuutioiden lukumäärä: \n"))      # inputti noppien määrälle
 
-    n = int(input("Kuinka monta kertaa haluat heittää noppaa? "))
-    for i in range(n):
-        print(f"Nopan luku: {random.randint(1,6)}")
-        noppien_summa.append
-        print(f"Noppien summa: {noppien_summa}")
+    if arpakuutioiden_lukum <= 0:       # ei voi olla negatiivinen
+        print("Arpakuutioiden lukumäärän pitää olla positiivinen.")
+
+    else:       # muuten heitetään nopat
+        tuloksen_summa = 0 # summa aluksi nolla
+        heitto = 1 # heitto aluksi yksi
+
+        for i in range(arpakuutioiden_lukum):
+            silmaluku = random.randint(1,6) # yksittäinen noppa
+            print(f"Heitto: {heitto} , Silmäluku: {silmaluku}") # ensimmäinen heitto on yksi, silmäluku on random jne.
+            tuloksen_summa += silmaluku
+            heitto += 1 # heitto kasvaa yhdellä joka kierroksella, siihen asti kun ollaan päästy haluttujen heittojen määrään
+        print(f"Heitettyjen arpakuutioiden summa: {tuloksen_summa}")
+
 useampi_arpakuutio()
+
