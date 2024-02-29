92250fec54c2a9d32fd99d9b3ae37abbceda3ae0
onnik <onni.kivinen@gmail.com>
2024-02-29 19:15:12 +0200

    Refactor code: Fix indentation and add new function for Moduuli 5 exercises

--- Oma/Moduuli 5 harjoitukset funktioina.py
+++ Oma/Moduuli 5 harjoitukset funktioina.py
@@ -6,13 +6,12 @@ def useampi_arpakuutio():
     
     import random
     
-    tuloksen_summa = 0      # alustettu nollaksi
-    arpakuutioiden_lukum = int(input("Anna arpakuutioiden lukumäärä: \n"))      # inputti noppien määrälle
+    tuloksen_summa = 0 # alustettu nollaksi
+    arpakuutioiden_lukum = int(input("Anna arpakuutioiden lukumäärä: \n")) # inputti noppien määrälle
 
-    if arpakuutioiden_lukum <= 0:       # ei voi olla negatiivinen
+    if arpakuutioiden_lukum <= 0: # ei voi olla negatiivinen
         print("Arpakuutioiden lukumäärän pitää olla positiivinen.")
-
-    else:       # muuten heitetään nopat
+    else: # muuten heitetään nopat
         tuloksen_summa = 0 # summa aluksi nolla
         heitto = 1 # heitto aluksi yksi
 
@@ -23,5 +22,32 @@ def useampi_arpakuutio():
             heitto += 1 # heitto kasvaa yhdellä joka kierroksella, siihen asti kun ollaan päästy haluttujen heittojen määrään
         print(f"Heitettyjen arpakuutioiden summa: {tuloksen_summa}")
 
-useampi_arpakuutio()
+# useampi_arpakuutio()
+
+
+# MODUULI 5, TEHTÄVÄ 2 luku_input != ""
+
+def viisi_suurinta_lukua():
+    luvut = [] 
+    luku_input = input("Anna luku: \n")
+    
+    while True:
+        if luku_input == "":
+            break
+        else:
+            luku_input = int(luku_input)
+            luvut.append(luku_input)
+            luku_input = input("Anna luku: \n")
+    
+    luvut.sort(reverse=True)
+    print("Viisi suurinta lukua ovat: ")
+    
+    for luku in range(0,5):
+        print(luvut[luku])
+
+viisi_suurinta_lukua()
+
+
+# MODUULI 5, TEHTÄVÄ 3
+
 
