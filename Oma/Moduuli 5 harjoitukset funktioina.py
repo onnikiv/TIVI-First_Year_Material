ace8900225a82ee7571d3623e312630d9a9bb1f3
onnik <onni.kivinen@gmail.com>
2024-02-29 16:35:46 +0200

    Refactor code to improve performance and readability

---
+++ Oma/Moduuli 5 harjoitukset funktioina.py
@@ -0,0 +1,11 @@
+def useampi_arpakuutio():
+    
+    import random
+    noppien_summa = []
+
+    n = int(input("Kuinka monta kertaa haluat heittää noppaa? "))
+    for i in range(n):
+        print(f"Nopan luku: {random.randint(1,6)}")
+        noppien_summa.append
+        print(f"Noppien summa: {noppien_summa}")
+useampi_arpakuutio()
