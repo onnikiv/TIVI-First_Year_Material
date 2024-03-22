9bad1b9f0122123347e2808f1c975f10b4bb4bd6
onnik <onni.kivinen@gmail.com>
2024-03-22 14:56:54 +0200

    Add new classes for employees and print their information

--- 1. Notes & Exercises/Harjoitukset ja muistiinpanot (Ohjelmisto2)/viikko1.3 ostosluokka.py
+++ 1. Notes & Exercises/Harjoitukset ja muistiinpanot (Ohjelmisto2)/viikko1.3 ostosluokka.py
@@ -178,8 +178,6 @@ dog1 = Dog("Dog", "Labrador")
 dog1.speak()
 
 
-'''
-
 class Henkilö:
     def __init__(self, nimi, ikä):
         self.nimi = nimi
@@ -201,3 +199,47 @@ opiskelija1 = Opiskelija("Rene", 7, 123456789)
 
 opiskelija1.esittely()
  
+ '''
+
+class Työntekijä:
+
+    työntekijöiden_lukumäärä = 0
+
+    def __init__(self, etunimi, sukunimi):
+        Työntekijä.työntekijöiden_lukumäärä = Työntekijä.työntekijöiden_lukumäärä + 1
+        self.työntekijänumero = Työntekijä.työntekijöiden_lukumäärä
+        self.etunimi = etunimi
+        self.sukunimi = sukunimi
+
+    def tulosta_tiedot(self):
+        print(f"{self.työntekijänumero}: {self.etunimi} {self.sukunimi}")
+
+class Tuntipalkkainen(Työntekijä):
+
+    def __init__(self, etunimi, sukunimi, tuntipalkka):
+        self.tuntipalkka = tuntipalkka
+        super().__init__(etunimi, sukunimi)
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f" Tuntipalkka: {self.tuntipalkka}")
+
+class Kuukausipalkkainen(Työntekijä):
+
+    def __init__(self, etunimi, sukunimi, kuukausipalkka):
+        self.kuukausipalkka = kuukausipalkka
+        super().__init__(etunimi, sukunimi)
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f" Kuukausipalkka: {self.kuukausipalkka}")
+
+
+työntekijät = []
+työntekijät.append(Tuntipalkkainen("Viivi", "Virta", 12.35))
+työntekijät.append(Kuukausipalkkainen("Ahmed", "Habib", 2750))
+työntekijät.append(Työntekijä("Pekka", "Puro"))
+työntekijät.append(Tuntipalkkainen("Olga", "Glebova", 14.92))
+
+for t in työntekijät:
+    t.tulosta_tiedot()
