70d3d99eb658d7f2ee88fe39a3bb6acf077bd24e
onnik <onni.kivinen@gmail.com>
2024-03-22 13:14:31 +0200

    finish

--- 1. Notes & Exercises/Harjoitukset ja muistiinpanot (Ohjelmisto2)/viikko1.3 ostosluokka.py
+++ 1. Notes & Exercises/Harjoitukset ja muistiinpanot (Ohjelmisto2)/viikko1.3 ostosluokka.py
@@ -30,7 +30,7 @@ student2 = Students("Maija", school2)
 print(f"'{student1.name}' opiskelee paikassa: '{student1.school.name}'.")
 print(f"'{student2.name}' opiskelee paikassa: '{student2.school.name}'.")
 
-'''
+
 
 class Book:
     def __init__(self, title):
@@ -66,3 +66,52 @@ for book in library1.books:
 print (f"\n'{library2.name}' sisältää kirjat:")
 for book in library2.books:
     print(book.title)
+
+'''
+
+
+class Student:
+    Student_list = []
+
+    def __init__(self, name):
+        self.name = name
+        self.courses = []
+
+    def enroll(self,course):
+        self.courses.append(course)
+        course.students.append(self)
+        
+
+
+class Course:
+
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+    
+student1 = Student("Pekka")
+student2 = Student("Maija")
+course1 = Course("Python 101")
+course2 = Course("Javascript 101")
+
+student1.enroll(course1)
+student1.enroll(course2)
+student2.enroll(course2)
+
+
+print(f"\n'{student1.name}' opiskelee kursseja:")
+for course in student1.courses:
+    print(course.name)
+
+print(f"\n'{student2.name}' opiskelee kursseja:")
+for course in student2.courses:
+    print(course.name)
+
+print(f"\n'{course1.name}' kurssilla opiskelee:")
+for student in course1.students:
+    print(student.name)
+
+print(f"\n'{course2.name}' kurssilla opiskelee:")
+for student in course2.students:
+    print(student.name)
+
