c9d6eb5183e8597c0b14b53934d2d114e90a8429
onnik <onni.kivinen@gmail.com>
2024-03-22 12:36:04 +0200

    tweaks

--- Python/Moduuli 10/Tehtävä 1 - Hissiluokka.py
+++ Python/Moduuli 10/Tehtävä 1 - Hissiluokka.py
@@ -1,17 +1,26 @@
 class Hissi:
     
-    def __init__(self, kerros=0):
+    def __init__(self, kerros, alin_kerros=0, ylin_kerros=10):
         self.kerros = kerros
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylin_kerros
 
-    def kerros_alas(self):
+    def kerros_alas(self, kerros):
         kerros = kerros - 1
-        print(f"Menty alaspäin.")
 
-    def kerros_ylos(self):
+    def kerros_ylos(self, kerros):
         kerros = kerros + 1
-        print(f"Menty ylöspäin.")
 
 
     def siirry_kerrokseen(self, kerros):
-        kerros_syote = int(input("Mihin kerrokseen haluat mennä? "))
-        
+        self.kerros = kerros
+        if self.kerros < self.alin_kerros:
+            self.kerros = self.alin_kerros
+        elif self.kerros > self.ylin_kerros:
+            self.kerros = self.ylin_kerros
+        else:
+            return f"Olet kerroksella {self.kerros}."
+        alaspain = Hissi.kerros_alas()
+        ylospain = Hissi.kerros_ylos()
+
+hissi = Hissi.siirry_kerrokseen(0, 5)
