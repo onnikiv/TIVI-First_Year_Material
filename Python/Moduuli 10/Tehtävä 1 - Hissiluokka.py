b9ad1ce507758425bec93946450eddb464eca91a
onnik <onni.kivinen@gmail.com>
2024-03-23 20:10:25 +0200

    Refactor Hissi class to use separate methods for moving up and down

--- Python/Moduuli 10/Tehtävä 1 - Hissiluokka.py
+++ Python/Moduuli 10/Tehtävä 1 - Hissiluokka.py
@@ -1,26 +1,26 @@
+
 class Hissi:
     
-    def __init__(self, kerros, alin_kerros=0, ylin_kerros=10):
-        self.kerros = kerros
-        self.alin_kerros = alin_kerros
-        self.ylin_kerros = ylin_kerros
+    def __init__(self):
+        self.pohja_kerros = 0
+        self.korkein_kerros = 5
+    
 
-    def kerros_alas(self, kerros):
-        kerros = kerros - 1
-
-    def kerros_ylos(self, kerros):
-        kerros = kerros + 1
-
-
-    def siirry_kerrokseen(self, kerros):
-        self.kerros = kerros
-        if self.kerros < self.alin_kerros:
-            self.kerros = self.alin_kerros
-        elif self.kerros > self.ylin_kerros:
-            self.kerros = self.ylin_kerros
+    def kerros_ylos(self):
+        if self.pohja_kerros < self.korkein_kerros:
+            self.pohja_kerros += 1
         else:
-            return f"Olet kerroksella {self.kerros}."
-        alaspain = Hissi.kerros_alas()
-        ylospain = Hissi.kerros_ylos()
+            print("Olet jo ylimmässä kerroksessa.")
+    
+    def kerros_alas(self):
+        if self.pohja_kerros > 0:
+            self.pohja_kerros -= 1
+        else:
+            print("Olet jo alimmassa kerroksessa.")
 
-hissi = Hissi.siirry_kerrokseen(0, 5)
+    def kerros(self):
+        print(f"Olet kerroksessa {self.pohja_kerros}.")
+
+    
+hissi1 = Hissi()
+hissi1.kerros_ylos(1)
