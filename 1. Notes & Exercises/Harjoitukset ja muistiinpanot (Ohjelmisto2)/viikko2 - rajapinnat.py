08cb66ce4e1ca52be6031f005dec0de5b99b5f61
onnik <onni.kivinen@gmail.com>
2024-03-26 11:05:29 +0200

    Fix API URL and handle exceptions in search_shows function

--- 1. Notes & Exercises/Harjoitukset ja muistiinpanot (Ohjelmisto2)/viikko2 - rajapinnat.py
+++ 1. Notes & Exercises/Harjoitukset ja muistiinpanot (Ohjelmisto2)/viikko2 - rajapinnat.py
@@ -1,20 +1,32 @@
 # Moduuli 12 esimerkit
-
+import json
 import requests
 
 
 def search_shows(search_query):
-    response = requests.get(f'http://api.tvmaze.com/search/shows?q={search_query}')
-    shows = response.json()
-    # print(f"Ensimmäinen sarja: {shows[0]['show']['name']}")
-    # Tulostetaan kaikki hakutulokset.
-    print(f"\nHaun tulokset hakusanalla: {search_query}")
-    for show in shows:
-        # haetaan genret sisäkkäisellä silmukalla
-        genres = ""
-        for genre in show['show']['genres']:
-            genres += genre + genre
-        print(f"Sarjan nimi:    {show['show']['name']} ({genres}): {show['show']['url']}")
+    url = f'https://api.tvmaze.com/search/shows?q={search_query}'
+    try: 
+        response = requests.get(url)
+        
+        print(f"HTTP response status code: {response.status_code}")
+        shows = response.json()
+        # print(f"Ensimmäinen sarja: {shows[0]['show']['name']}")
+        # Tulostetaan kaikki hakutulokset, jos HTTP-pyyntö onnistui
+        if response.status_code == 200:
+            print(f"\nHaun tulokset hakusanalla: {search_query}")
+            for show in shows:
+                # haetaan genret sisäkkäisellä silmukalla
+                genres = ""
+                for genre in show['show']['genres']:
+                    genres += genres + genre + " "
+                print(f"Sarjan nimi:    {show['show']['name']} ({genres}): {show['show']['url']}")
+            print("\n")
+
+    except requests.exceptions.RequestException as error:
+        print("HTTP-pyynnössä tapahtui virhe. Ei verkkoyhteyttä palvelimeen.")
+        print(f"Virheilmoitus: {error}")
+    
+
 
 
 search_input = input("Anna hakusana:\n")
