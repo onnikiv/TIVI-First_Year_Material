9147b6547ec39bf2cf206ef6175f1dad1a94b2fe
onnik <onni.kivinen@gmail.com>
2024-04-02 11:03:31 +0300

    Add support for multiply calculation in Flask app

--- 1. Notes & Exercises/Harjoitukset ja muistiinpanot (Ohjelmisto2)/viikko3 - flask.py
+++ 1. Notes & Exercises/Harjoitukset ja muistiinpanot (Ohjelmisto2)/viikko3 - flask.py
@@ -1,22 +1,36 @@
 # Mod 13 - Python-palvelinesimerkki Flaskilla
 
-from flask import Flask, request
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
-def calc_sum(a, b):
+def sum(a, b):
     return a + b
 
-@app.route('/calc/sum')
-def calculate():
+def multiply(a, b):
+    return a * b
+
+@app.route('/calc/<type>')
+def calculate(type):
     # print(request.args.get('num1'))
     try:
         num1 = float(request.args.get('num1'))
         num2 = float(request.args.get('num2'))
-        return str(calc_sum(num1, num2))
+        
+        if type == 'sum':
+            result = sum(num1, num2) # http://localhost:3000/calc/sum?num1=3&num2=5
+        
+        elif type == 'multiply':
+            result = multiply(num1, num2) # http://localhost:3000/calc/multiply?num1=3&num2=5
+
+        else:
+            
+            response_body = {'error': 'Unknown calculation type.', 'status': 400}
+            return Response(response=response_body, status=400)
+        return {'result': result, "numbers": [num1, num2]}
     
     except:
-        return 'Invalid parameters.'
+        return {'error': 'Invalid parameters.', 'status': 400}
 
 
 
