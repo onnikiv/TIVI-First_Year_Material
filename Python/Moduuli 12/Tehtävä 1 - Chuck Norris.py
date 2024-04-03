866cff4a67ee8ee8b68a7e10e135731357933cea
onnik <onni.kivinen@gmail.com>
2024-04-03 14:25:28 +0300

    finish

---
+++ Python/Moduuli 12/Tehtävä 1 - Chuck Norris.py
@@ -0,0 +1,12 @@
+# Moduuli 12 esimerkit
+
+import requests
+
+
+def chuck_norris_vitsi():
+    url = 'https://api.chucknorris.io/jokes/random'
+    response = requests.get(url)
+    vitsi = response.json()
+    print(f"\n{vitsi['value']}")
+
+chuck_norris_vitsi()
