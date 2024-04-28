509382ceb2191694b45047092101109366edcfe9
onnik <onni.kivinen@gmail.com>
2024-04-28 16:33:55 +0300

    omaa

--- Harjoitukset/Ohjelmisto 2 - Oma/luokkaharkat.py
+++ Harjoitukset/Ohjelmisto 2 - Oma/luokkaharkat.py
@@ -1,10 +1,17 @@
 class Auto:
     
-    def __init__(self, rekisteri_nro, max_nopeus, kulutettu_matka):
+    def __init__(self, rekisteri_nro, max_nopeus, ajettu_matka=0):
         self.rekisteri_nro = rekisteri_nro
         self.max_nopeus = max_nopeus
-        self.kulutettu_matka = kulutettu_matka
+        self.ajettu_matka = ajettu_matka
 
     def aja_matka(self, matka):
-        super().__init__(self)
-        self.matka = matka
+        self.ajettu_matka += matka
+    
+    def ominaisuudet(self):
+         print(f"Rekisteri: {self.rekisteri_nro} --- Max nopeus: {self.max_nopeus} --- Ajettu matka: {self.ajettu_matka} ")
+                
+auto1 = Auto("ABC-123", 100)
+auto1.ominaisuudet()
+auto1.aja_matka(235)
+auto1.ominaisuudet()
